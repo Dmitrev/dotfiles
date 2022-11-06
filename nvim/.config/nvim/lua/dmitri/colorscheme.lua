45cd521b3759202b505ac0184b4b589e84c18e38
Dmitri Chebotarev <dmitri.chebotrev@gmail.com>
2022-11-06 17:17:34 +0000

    set nvim theme to catppuccin

--- nvim/.config/nvim/lua/dmitri/colorscheme.lua
+++ nvim/.config/nvim/lua/dmitri/colorscheme.lua
@@ -1,4 +1,4 @@
-local status_ok, _ = pcall(vim.cmd, "colorscheme tokyonight-night")
+local status_ok, _ = pcall(vim.cmd, "colorscheme catppuccin-mocha")
 
 if not status_ok then 
     vim.notify("Could not load colorscheme");
