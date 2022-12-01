cd64f10ac173eba2be687d8a07a071c8fc14617e
Dmitri Chebotarev <dmitri.chebotrev@gmail.com>
2022-12-01 15:05:46 +0000

    change theme to gruvbox

--- nvim/.config/nvim/lua/dmitri/colorscheme.lua
+++ nvim/.config/nvim/lua/dmitri/colorscheme.lua
@@ -1,21 +1,24 @@
--- local theme = 'onedark';
- -- local dark = true;
- --
- -- if not dark then
- --     local status_ok, _ = pcall(vim.cmd, "set background=light")
- --     if not status_ok then 
- --         vim.notify("Could not set background to light");
- --     end
- -- end
- --
- -- local status_ok, _ = pcall(vim.cmd, "colorscheme "..theme)
- --
- -- if not status_ok then 
- --     vim.notify("Could not load colorscheme");
- -- end
+local theme = 'gruvbox';
+local dark = true;
 
-require('onedark').setup {
-    style = 'deep'
-}
-require('onedark').load()
+ if not dark then
+     local status_ok, _ = pcall(vim.cmd, "set background=light")
+     if not status_ok then
+         vim.notify("Could not set background to light");
+     end
+ end
+
+ local status_ok, _ = pcall(vim.cmd, "colorscheme "..theme)
+
+ if not status_ok then
+     vim.notify("Could not load colorscheme");
+ end
+
+-- require('onedark').setup {
+--     style = 'deep',
+--     highlights = {
+--         ['@visibility_modifier'] = {fg = '$purple'}
+--     }
+-- }
+-- require('onedark').load()
 
