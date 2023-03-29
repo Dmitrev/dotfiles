899979ffed10f85db099789e8082ee6e8d98f51e
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2023-03-29 10:27:57 +0100

    added wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+config.font = wezterm.font 'PragmataPro Mono Liga'
+config.font_size = 16
+return config
