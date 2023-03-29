8f50c10bb5955a0ff3bb5dd30869d89a646c4f30
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2023-03-29 11:42:53 +0100

    fixed font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = {}
 if wezterm.config_builder then
     config = wezterm.config_builder()
 end
-config.font = wezterm.font 'PragmataPro Mono Liga'
+config.font = wezterm.font("PragmataPro", {weight="Regular", stretch="Normal", style="Normal"})
 config.color_scheme = 'Catppuccin Mocha'
 config.font_size = 16
 return config
