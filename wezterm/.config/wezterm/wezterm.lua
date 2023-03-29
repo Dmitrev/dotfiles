c9748d16ccf258cde9a73604fcec48404d6a9659
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2023-03-29 10:30:59 +0100

    updated theme wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,5 +6,6 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 config.font = wezterm.font 'PragmataPro Mono Liga'
+config.color_scheme = 'Catppuccin Mocha'
 config.font_size = 16
 return config
