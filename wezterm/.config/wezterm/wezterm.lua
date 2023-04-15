d0c195f4ca3296ff2e43e83a1d5ad1fbd2d364a5
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2023-04-15 18:47:08 +0100

    wezterm experiments

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,7 +5,15 @@ local config = {}
 if wezterm.config_builder then
     config = wezterm.config_builder()
 end
-config.font = wezterm.font("PragmataPro", {weight="Regular", stretch="Normal", style="Normal"})
+
+config.font = wezterm.font(
+    "PragmataPro",
+    { weight = 'Regular' }
+)
+
+config.freetype_load_target = 'Light'
+config.freetype_render_target = 'HorizontalLcd'
+
 config.color_scheme = 'Catppuccin Mocha'
-config.font_size = 16
+config.font_size = 14
 return config
