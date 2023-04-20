d3bae9fdd40a30be2bc5eed5cae6bf6beff6745c
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2023-04-20 12:46:54 +0100

    Updated meg script to start tmux

--- scripts/scripts/meg.sh
+++ scripts/scripts/meg.sh
@@ -2,34 +2,43 @@
 session='meg'
 dir="$HOME/sites/meg"
 
-# Open NeoVim
-window=0
-tmux new -d -c $dir -s $session 
-tmux split-window -t $session:$window.0 -h -c $dir
-tmux split-window -t $session:$window.0 -v -c $dir 
-tmux split-window -t $session:$window.2 -v -c $dir 
-tmux send-keys -t $session:$window.3 'docker compose up --build' C-m
-tmux send-keys -t $session:$window.2 'docker stats' C-m
+tmux has-session -t $session 2>/dev/null
+if [ $? != 0 ]; then
+    tmux new -s $session -c $dir -d
 
-# these commands depend on Docker running
-while ! docker exec meg-php-1 php --version; do sleep 1; done
+    # Open NeoVim
+    window=1
+    tmux new-window -t $session:$window -n 'nvim' -c $dir 
+    tmux rename-window -t $session:$window 'nvim'
+    tmux send-keys -t $session:$window 'nvim' C-m # C-m executes
 
-tmux send-keys -t $session:$window.0 'make web' C-m
-tmux send-keys -t $session:$window.1 'make queue' C-m
+    # Open docker related windows
+    window=2
+    tmux new-window -t $session:$window -n 'git' -c $dir
+    tmux send-keys -t $session:$window 'lazygit' C-m
 
-window=1
-tmux new-window -t $session:$window -n 'nvim' -c $dir 
-tmux rename-window -t $session:$window 'nvim'
-tmux send-keys -t $session:$window 'nvim' C-m # C-m executes
+    window=3
+    tmux new-window -t $session:$window -n 'docker' -c $dir
+    tmux split-window -t $session:$window.1 -h -c $dir
+    tmux split-window -t $session:$window.1 -v -c $dir 
+    tmux split-window -t $session:$window.3 -v -c $dir 
+    tmux send-keys -t $session:$window.4 'docker compose up --build' C-m
+    tmux send-keys -t $session:$window.3 'docker stats' C-m
 
-# Open docker related windows
-window=2
-tmux new-window -t $session:$window -n 'git' -c $dir
-tmux send-keys -t $session:$window 'lazygit' C-m
+    # # these commands depend on Docker running
+    while ! docker exec meg-php-1 php --version; do sleep 1; done
 
-window=3
-tmux new-window -t $session:$window -n 'workspace' -c $dir
-tmux send-keys -t $session:$window 'make enter' C-m
+    tmux send-keys -t $session:$window.1 'make web' C-m
+    tmux send-keys -t $session:$window.2 'make queue' C-m
+
+
+    window=4
+    tmux new-window -t $session:$window -n 'workspace' -c $dir
+    tmux send-keys -t $session:$window 'make enter' C-m
+
+    # swap window 3 & 4
+    tmux swap-window -s 3 -t 4
+fi
 
 # Get into the session
 tmux attach -t $session
