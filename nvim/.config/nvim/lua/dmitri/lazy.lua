1b5d53877bda23881a14a46a90f6b1d852833225
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2023-11-10 10:55:59 +0000

    Updated telescope

--- nvim/.config/nvim/lua/dmitri/lazy.lua
+++ nvim/.config/nvim/lua/dmitri/lazy.lua
@@ -127,8 +127,8 @@ require("lazy").setup({
     -- telescope and extensions
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
+        -- tag = '0.1.1',
+        branch = '0.1.x',
         dependencies = { {'nvim-lua/plenary.nvim'} }
     },
     {'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
