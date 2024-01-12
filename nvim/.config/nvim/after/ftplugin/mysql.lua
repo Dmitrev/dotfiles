774d8ff236b8769fa5e16d12c7ecf41afc191518
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2024-01-12 16:51:28 +0000

    Added some mysql bindings

---
+++ nvim/.config/nvim/after/ftplugin/mysql.lua
@@ -0,0 +1,6 @@
+print('is this running?');
+local keymap = vim.keymap;
+keymap.set('n', '<leader>r', ':normal vip<CR><PLUG>(DBUI_ExecuteQuery)', { buffer = true })
+keymap.set('n', '<leader>F', ':%!sql-formatter-cli .<CR>', { buffer = true })
+keymap.set('n', '<leader>f', ':normal vip<CR>:!sql-formatter-cli<CR>', { buffer = true })
+keymap.set('n', '<leader>w', '<PLUG>(DBUI_SaveQuery)', { buffer = true })
