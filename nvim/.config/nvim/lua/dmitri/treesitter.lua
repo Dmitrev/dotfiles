2253023dabdc10644bbed3f15ce4fd3efc60e134
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2024-08-09 19:07:29 +0100

    bunch of stuff

--- nvim/.config/nvim/lua/dmitri/treesitter.lua
+++ nvim/.config/nvim/lua/dmitri/treesitter.lua
@@ -36,10 +36,10 @@ ntc.setup {
     -- disable = { "c", "rust" },
     -- -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
     disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
+        local max_filesize = 200 * 1024 -- 100 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
-            print("Auto disabled treesitter, filesize > 100KB")
+            print("Auto disabled treesitter, filesize > 200KB")
             return true
         end
     end,
