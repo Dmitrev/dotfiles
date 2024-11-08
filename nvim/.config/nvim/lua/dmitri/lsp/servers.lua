0e0da7804a383d56e0bdf091fb5a146e1b2d8ada
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2024-11-08 22:08:49 +0000

    updated colors again lol

--- nvim/.config/nvim/lua/dmitri/lsp/servers.lua
+++ nvim/.config/nvim/lua/dmitri/lsp/servers.lua
@@ -62,14 +62,14 @@ local lsp_servers = {
          }
       }
    },
-   -- volar = {
-   --    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-   --    init_options = {
-   --       vue = {
-   --          hybridMode = false,
-   --       },
-   --    },
-   -- }, -- vuejs
+   volar = {
+      filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+      init_options = {
+         vue = {
+            hybridMode = false,
+         },
+      },
+   }, -- vuejs
    rust_analyzer = {},
    cssls = {},
    gopls = {},
