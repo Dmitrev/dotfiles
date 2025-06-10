712551f08462c7a337919494e1907df96d36d42c
Dmitri Chebotarev <dmitri.chebotarev@goodlord.co>
2025-06-10 18:49:32 +0100

    alacritty is the best after all

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,10 +7,10 @@ if wezterm.config_builder then
 end
 --
 config.font = wezterm.font_with_fallback {
-    -- { family = 'Iosevka Fixed', weight = "Regular" },
+    { family = 'Iosevka Fixed', weight = "Regular" },
     -- { family = 'IosevkaNerdFont', weight = "Regular" },
     -- { family = 'IosevkaTermNerdFont', weight = "Regular" },
-    { family = 'IosevkaTermSlabNerdFont', weight = "Regular" },
+    -- { family = 'IosevkaTermSlabNerdFont', weight = "Regular" },
     "Symbols Nerd Font"
 }
 --
