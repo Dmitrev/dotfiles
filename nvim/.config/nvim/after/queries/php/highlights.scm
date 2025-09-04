020449152642015137dfe61f8f65d6128d74323f
Dmitri Chebotarev <dmitri.chebotarev@goodlord.co>
2025-09-04 23:51:10 +0100

    Fix php highlights argument name

--- nvim/.config/nvim/after/queries/php/highlights.scm
+++ nvim/.config/nvim/after/queries/php/highlights.scm
@@ -1,7 +1,6 @@
 ;; extends
 ;; Capture the name field of an argument in object creation
-(object_creation_expression
-  (arguments
-    (argument
-      name: (name) @constructor.param.name)))
+(arguments
+  (argument
+    name: (name) @constructor.param.name))
 
