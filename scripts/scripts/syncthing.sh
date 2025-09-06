c495531812bd17f54891515c235a747a12936ec8
Dmitri <dmitri.chebotarev@gmail.com>
2025-09-06 11:57:13 +0100

    fixed autostart syncthing

--- scripts/scripts/syncthing.sh
+++ scripts/scripts/syncthing.sh
@@ -1,32 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
-LOCAL_SYNCTHING_BIN="$HOME/.local/bin/syncthing"
-GLOBAL_SYNCTHING_BIN="syncthing"
-SYNCTHING_SESSION="syncthing"
-SCRIPT_NAME=$(basename "$0")
-
-if command -v "$GLOBAL_SYNCTHING_BIN" 2>&1 > /dev/null; then
-    echo "syncthing found"
-    SYNCTHING_BIN="$GLOBAL_SYNCTHING_BIN"
-else
-    if command -v "$LOCAL_SYNCTHING_BIN" 2>&1 > /dev/null; then
-        SYNCTHING_BIN="$LOCAL_SYNCTHING_BIN"
-    else
-        echo "syncthing not found"
-        exit 1
-    fi
+if [ -f "$HOME/.zshrc" ]; then
+    source "$HOME/.zshrc"
 fi
 
-if tmux has-session -t "$SYNCTHING_SESSION" 2>/dev/null; then
-    echo "session exists"
-else
-    tmux new -s "$SYNCTHING_SESSION" -d
-fi
-
-if ps aux | grep syncthing | grep -v grep | grep -v tmux | grep -v "$SCRIPT_NAME"; then
-    echo "syncthing is running"
-else
-    echo "syncthing is not running"
-    tmux send-keys -t "$SYNCTHING_SESSION:^" "$SYNCTHING_BIN --no-browser" C-m
-fi
+tmux new -d -s syncthing -- "$HOME/.local/bin/syncthing" --no-browser
 
