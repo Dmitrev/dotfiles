39b185bbeb6d2dd20ea1f3298ee28c67e2f82e4f
Dmitri <dmitri.chebotarev@gmail.com>
2025-12-11 09:00:08 +0000

    cat

--- nvim/.config/nvim/lua/dmitri/colorscheme.lua
+++ nvim/.config/nvim/lua/dmitri/colorscheme.lua
@@ -1,10 +1,10 @@
 -- Gruber overrides
 
-vim.api.nvim_set_hl(0, "@property", {link = "GruberDarkerFg1"})
-vim.api.nvim_set_hl(0, "@function", {link = "GruberDarkerFg1"})
-vim.api.nvim_set_hl(0, "@module", {link = "GruberDarkerFg1"})
-vim.api.nvim_set_hl(0, "@type", {link = "GruberDarkerFg1"})
-vim.api.nvim_set_hl(0, "@type.definition", {link = "GruberDarkerFg1"})
+-- vim.api.nvim_set_hl(0, "@property", {link = "GruberDarkerFg1"})
+-- vim.api.nvim_set_hl(0, "@function", {link = "GruberDarkerFg1"})
+-- vim.api.nvim_set_hl(0, "@module", {link = "GruberDarkerFg1"})
+-- vim.api.nvim_set_hl(0, "@type", {link = "GruberDarkerFg1"})
+-- vim.api.nvim_set_hl(0, "@type.definition", {link = "GruberDarkerFg1"})
 
 -- JSON
-vim.api.nvim_set_hl(0, "@property.json", {link = "String", italic = false})
+-- vim.api.nvim_set_hl(0, "@property.json", {link = "String", italic = false})
--- nvim/.config/nvim/lua/dmitri/lazy.lua
+++ nvim/.config/nvim/lua/dmitri/lazy.lua
@@ -298,23 +298,57 @@ require("lazy").setup({
             -- vim.cmd("colorscheme gruvbox-material")
         end
     },
+    -- {
+    --     "blazkowolf/gruber-darker.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --
+    --     config = function()
+    --         require("gruber-darker").setup(
+    --             {
+    --                 italic = {
+    --                     strings = false,
+    --                     operators = false,
+    --                 }
+    --             }
+    --         )
+    --         vim.cmd("colorscheme gruber-darker")
+    --     end
+    --
+    -- },
     {
-        "blazkowolf/gruber-darker.nvim",
+        'AlexvZyl/nordic.nvim',
         lazy = false,
         priority = 1000,
-
         config = function()
-            require("gruber-darker").setup(
-                {
-                    italic = {
-                        strings = false,
-                        operators = false,
-                    }
-                }
-            )
-            vim.cmd("colorscheme gruber-darker")
+            -- require('nordic').load()
         end
-
+    },
+    {
+        "oxfist/night-owl.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+            -- require("night-owl").setup()
+            -- vim.cmd.colorscheme("night-owl")
+        end
+    },
+    {
+        "NTBBloodbath/sweetie.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- vim.cmd.colorscheme("sweetie")
+        end
+    },
+    { 
+        "catppuccin/nvim", 
+        name = "catppuccin", 
+        priority = 1000, 
+        config = function()
+            vim.cmd.colorscheme("catppuccin-mocha")
+        end
     },
     {
         "catgoose/nvim-colorizer.lua",
