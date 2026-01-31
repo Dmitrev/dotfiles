3b16aaed1f0fcfdc15d6fae4459cfd3ea21346e1
Dmitri <dmitri@pop-os.localdomain>
2026-01-31 11:38:35 +0000

    install btop

--- scripts/scripts/install-btop.sh
+++ scripts/scripts/install-btop.sh
@@ -1,18 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-sudo apt install coreutils sed git build-essential lowdown
+set -euo pipefail
+
+# dependencies
+# install-gcc
+# install-cmake
+# install-ninja-build
+# install-lowdown
 
 CLONE_DIR="/tmp/btop"
-INSTALL_DIR="$HOME/.local"
+INSTALL_DIR="$HOME/.local/apps/btop"
 CURRENT_DIR=$(pwd)
 
 rm -rf "$CLONE_DIR"
-rm -rf "$INSTALL_DIR/btop"
+rm -rf "$INSTALL_DIR"
 git clone https://github.com/aristocratos/btop.git "$CLONE_DIR"
 
 cd "$CLONE_DIR"
-make DEBUG=true CXX=gcc
-make install PREFIX="$INSTALL_DIR" 
-make setcap PREFIX="$INSTALL_DIR" 
-sudo make setuid PREFIX="$INSTALL_DIR" 
-cd "$CURRENT_DIR"
+cmake -B build -G Ninja \
+  -DCMAKE_INSTALL_PREFIX=/tmp/test \
+  -DCMAKE_CXX_COMPILER=g++ \
+  -DCMAKE_CXX_STANDARD=23 \
+  -DCMAKE_CXX_STANDARD_REQUIRED=ON \
+  -DCMAKE_CXX_EXTENSIONS=OFF
+
+cmake --build build
+cmake --install build
