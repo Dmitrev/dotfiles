88c15e53f0e2d185bcd0c07d61541b0e03e08a1f
Dmitri Chebotarev <dmitri.chebotarev@goodlord.co>
2026-07-29 20:29:54 +0100

    emacs config

---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,13 @@
+; Keys for zooming and out
+(keymap-global-set "C-+" #'text-scale-increase)
+(keymap-global-set "C--" #'text-scale-decrease)
+
+(set-face-attribute 'default nil
+		    :family "Iosevka"
+		    :height 180)
+
+(load-theme 'modus-vivendi t)
+
+		  
+
+
